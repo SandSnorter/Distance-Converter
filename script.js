0e1b46afb5ed3527ffeb37c08e1cc5387be70b3c
Pallavi Shamkuwar <shamkuwarpallavi@gmail.com>
2023-11-03 07:50:21 +0530

    distance converter contains 3 files

---
+++ script.js
@@ -0,0 +1,41 @@
+(function(){
+
+    "use strict";
+
+    let convertType = "miles";
+    const heading = document.querySelector('h1');
+    const intro = document.querySelector('p');
+    const answer = document.getElementById('answer');
+    const form = document.getElementById('convert');
+
+    document.addEventListener('keydown',function(event){
+        var key = event.code;
+        if (key==='KeyK') {
+            convertType = 'kilometers';
+            heading.innerHTML = 'Kilometers to Miles Converter';
+            intro.innerHTML = 'Type in a number of kilometers and click the button to convert the distance to miles.';
+        }else if (key==='KeyM') {
+            convertType = 'miles';
+            heading.innerHTML = 'Miles to Kilometers Converter';
+            intro.innerHTML = 'Type in a number of miles and click the button to convert the distance to kilometers.';
+        }
+    });
+
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+
+        const distance = parseFloat(document.getElementById('distance').value);
+
+        if(distance){
+            if (convertType=="miles") {
+                var conversion = (distance*1.609344).toFixed(3);
+                answer.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`;
+            }else{
+                var conversion = (distance*0.621371192).toFixed(3);
+                answer.innerHTML = `<h2>${distance} kilometers converts to ${conversion} miles</h2>`;
+            }
+        }else{
+            answer.innerHTML = '<h2>Please provide a number!</h2>';
+        }
+    });
+})();
